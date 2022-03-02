fd74020c12c8d6024f8ceeebf3324139ca94e2c3
pivin <pivin@codeitup.ru>
2022-03-02 13:06:02 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='promo_api',
+    version='',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description='',
+    python_requires='==3.10.*'
+)
