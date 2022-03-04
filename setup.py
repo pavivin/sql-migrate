ad64627bc32e20501db059fb823d740ef39e7b83
pivin <pivin@codeitup.ru>
2022-03-04 09:42:24 +0300

    config

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='promo_api',
-    version='',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    url='',
-    license='',
-    author='',
-    author_email='',
-    description='',
-    python_requires='==3.10.*'
+    name="sql-automigrate",
+    version="",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    url="",
+    license="",
+    author="pavivin",
+    author_email="pavivin@yandex.ru",
+    description="",
+    python_requires=">=3.7.*",
 )
